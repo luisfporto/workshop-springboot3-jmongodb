66bbda6c6547253f6a2e20a4acf4b1e416677550
Luis Porto <luisporto1717@gmail.com>
2024-01-14 21:40:37 +0000

    User insertion with POST

--- src/main/java/com/lp/workshopmongo/resources/UserResource.java
+++ src/main/java/com/lp/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.lp.workshopmongo.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,8 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.lp.workshopmongo.DTO.UserDTO;
 import com.lp.workshopmongo.domain.User;
@@ -26,11 +30,23 @@ public class UserResource {
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
 	}
-	
+
 	@GetMapping("/{id}")
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
-		/*Path variable indica que o ID recebido como argumento deve casar com o id utilizado na URI, em GetMapping.*/
+		/*
+		 * Path variable indica que o ID recebido como argumento deve casar com o id
+		 * utilizado na URI, em GetMapping.
+		 */
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+
+		User obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
